b0da9a1d364c4a2803efbd7b30f999769e80eaa6
tennitien <ntctien.forwork@gmail.com>
2023-09-14 19:36:31 +0700

    login-authenticate

--- myAuthen/login/views.py
+++ myAuthen/login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from django.contrib.auth import authenticate
 # Create your views here.
 
 
@@ -11,4 +12,7 @@ class IndexClass(View):
     def post(self,req):
         username=req.POST.get('username')
         password=req.POST.get('password')
+        my_user= authenticate(username=username,password=password)
+        if my_user is None:
+            return HttpResponse('not have user')
         return HttpResponse(f'KQ: username: {username},pass: {password}')
