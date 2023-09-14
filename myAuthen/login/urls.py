33eb35add4facd4c90a2ea9d25905bf3bb26d2fe
tennitien <ntctien.forwork@gmail.com>
2023-09-14 20:10:03 +0700

    phan-quyenuser-ViewUser

--- myAuthen/login/urls.py
+++ myAuthen/login/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('',views.IndexClass.as_view(),name='index'),
+    path('user/',views.ViewUser.as_view(),name='user'),
 ]
--- myAuthen/login/views.py
+++ myAuthen/login/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate,login
 # Create your views here.
 
 
@@ -13,6 +13,18 @@ class IndexClass(View):
         username=req.POST.get('username')
         password=req.POST.get('password')
         my_user= authenticate(username=username,password=password)
+        
         if my_user is None:
             return HttpResponse('not have user')
-        return HttpResponse(f'KQ: username: {username},pass: {password}')
+        
+        login(req,my_user)
+        return render(req, 'login/home.html')
+    
+
+class ViewUser(View):
+    def get(self,req):
+        if not req.user.is_authenticated:
+            return HttpResponse('Vui long dang nhap')
+        else:
+            return HttpResponse('Ban da dang nhap r')
+        
