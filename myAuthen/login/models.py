a0d75d5ef9b8edc69f51e5a2944d61c5b421a4cd
tennitien <ntctien.forwork@gmail.com>
2023-09-15 22:17:42 +0700

    custom-user

--- myAuthen/login/models.py
+++ myAuthen/login/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
     title= models.CharField(max_length=100)
@@ -7,3 +7,4 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+    
--- mycustomuser/authuser/admin.py
+++ mycustomuser/authuser/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from . import models
+from .models import User
 # Register your models here.
-admin.site.register(models.Post)
+admin.site.register(User)
